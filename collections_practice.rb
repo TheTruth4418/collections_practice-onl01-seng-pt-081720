101c1935ee7a8a9eff6ee1f0b60ca89c242184a8
William West <wwest466@gmail.com>
2020-08-25 16:07:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -6,8 +6,8 @@ def sort_array_desc(arr)
   arr.reverse
 end
 
-def sort_array_asc(arr)
+def sort_array_char_count(arr)
   arr.sort do |a,b|
-    a.length <=> b.legnth
+    a.length <=> b.length
   end
 end
